1bbe1b2fc1785c5afcb3b2e800571e13439490f3
Javier <javdevia@gmail.com>
2023-08-11 18:31:24 -0400

    Adapter y ViewHolder implementados

--- app/src/main/java/com/project/affirmations/MainActivity.kt
+++ app/src/main/java/com/project/affirmations/MainActivity.kt
@@ -2,10 +2,14 @@ package com.project.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.affirmations.model.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+        
     }
 }
--- app/src/main/java/com/project/affirmations/model/data/Datasource.kt
+++ app/src/main/java/com/project/affirmations/model/data/Datasource.kt
@@ -5,7 +5,7 @@ import com.project.affirmations.model.Affirmation
 
 class Datasource {
 
-    fun loadAffirmations(): List<Affirmation>{
+    fun loadAffirmations(): List<Affirmation> {
         return listOf (
             Affirmation(R.string.affirmation1),
             Affirmation(R.string.affirmation2),
