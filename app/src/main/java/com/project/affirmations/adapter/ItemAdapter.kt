876a251bdc5bad12bf8aad5f6679aa8db721f8e5
Javier <javdevia@gmail.com>
2023-08-12 19:09:33 -0400

    Se agrega imagenes a cada objeto del RecyclerView

--- app/src/main/java/com/project/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/project/affirmations/adapter/ItemAdapter.kt
@@ -4,20 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.project.affirmations.R
 import com.project.affirmations.model.Affirmation
 
-class ItemAdapter (private val context: Context, private val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder> () {
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
-    val textView: TextView = view.findViewById(R.id.tvAffirmation)
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.tvAffirmation)
+        val imageView: ImageView = view.findViewById(R.id.ivImage)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
@@ -26,5 +30,6 @@ class ItemAdapter (private val context: Context, private val dataset: List<Affir
     override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
